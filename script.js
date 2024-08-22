b43fdf204a4690a0f98a5b3594c342b28fbba0a6
Jallaldin Hamdard <jalladinhamdard@gmail.com>
2024-08-22 17:02:22 +0100

    Changing display bar upon interaction with Buttons

--- script.js
+++ script.js
@@ -1,4 +1,9 @@
-
+let num1 = '';
+let num2 = '';
+let operator = '';
+let result = '';
+let display = document.querySelector('.display');
+const buttons = document.querySelectorAll('button');
 
 const add = function( a, b ) {
     return a + b;
@@ -17,9 +22,42 @@ const divide = function ( a , b){
     return a / b;
 }
 
-let number1, number2, operator;
 
-const operate = function (num1, num2, operator){
-    return add(num1, num2)
+const operate = function (num1, num2, operator){   
+    switch(operator){
+        case '+':
+            return add(num1, num2);
+            break;
+        case '-':
+            return subtract(num1, num2);
+            break;
+        case '*':
+            return multiply(num1, num2);
+            break;
+        case '/':
+            return divide(num1, num2);
+            break;
+    }
 }
+
+function changeDisplay(value) {
+    display.textContent = value;
+}
+
+buttons.forEach((button) => {
+    button.addEventListener('click', () => {
+        let displayValue = button.textContent;
+    })
+    if(displayValue >= '0' && displayValue <= '9'){
+        if(operator){
+            num2 += displayValue;
+            changeDisplay(num2);
+        }
+        else {
+            num1 += displayValue;
+            changeDisplay(num1);
+        }
+    }
+})
+
 
