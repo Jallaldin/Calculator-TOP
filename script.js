3cbc389b3fb1eae8df6cb9a3d15a5d9d4b0290ee
Jallaldin Hamdard <jalladinhamdard@gmail.com>
2024-08-22 15:18:44 +0100

    Numbers + Operations buttons made , UI complete

---
+++ script.js
@@ -0,0 +1,25 @@
+
+
+const add = function( a, b ) {
+    return a + b;
+}
+
+const subtract = function ( a, b ) {
+    return a  - b;
+}
+
+
+const multiply = function ( a, b){
+    return a * b;
+}
+
+const divide = function ( a , b){
+    return a / b;
+}
+
+let number1, number2, operator;
+
+const operate = function (num1, num2, operator){
+    return add(num1, num2)
+}
+
